5efff68a40ab4f8dbe61a36d95237f6d1d9d76be
joeyjojo12 <joeyjojo12@gmail.com>
2014-04-08 22:14:18 -0500

    UI commit

--- PeruPeople/src/database/MatrixUI.py
+++ PeruPeople/src/database/MatrixUI.py
@@ -7,39 +7,30 @@ class MatrixPanel(wx.Panel):
         wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
         
 
-        label1 = wx.statictext(self, label="type of minister :")
+        label1 = wx.StaticText(self, label="Type of minister :")
         
-        Consulter = self.societal = wx.checkbox(self, -1, "Consulter")
-        Huaca = self.societal = wx.checkbox(self, -1, "Huaca")
-        Malqui = self.societal = wx.checkbox(self, -1, "Malqui/Muñaos")
-        Lightning = self.societal = wx.checkbox(self, -1, "Lightning")
-        Sun = self.Societal = wx.CheckBox(self, -1, "Sun")
-        Capycocha = self.Societal = wx.CheckBox(self, -1, "Capycocha")
-        OtherConsulter = self.Societal = wx.CheckBox(self, -1, "Other")
-
-        Diviners = self.societal = wx.checkbox(self, -1, "Diviners")
-        Spiders = self.societal = wx.checkbox(self, -1, "Spiders (Ex: Pacharicucc or Pachacaricc or Pachacucc)")
-        Molle = self.societal = wx.checkbox(self, -1, "Molle (Ex: Rapiac)")
-        Love = self.societal = wx.checkbox(self, -1, "Love")
-        LostThings = self.Societal = wx.CheckBox(self, -1, "Lost things (Ex. Moscocc)")
-        Mushrooms = self.Societal = wx.CheckBox(self, -1, "Mushrooms")
- 
-        CuyExaminers = self.societal = wx.checkbox(self, -1, "Cuy Examiners (Ex. Hacaricucc o Cuyricucc)")
-        Purpose = self.societal = wx.checkbox(self, -1, "Purpose")
-        
-        Curer = self.societal = wx.checkbox(self, -1, "Curer (Ex: Macsa or Viha)")
-
-        Confessor = self.societal = wx.checkbox(self, -1, "Confessor (Ex: Aucachic/Ychuris in Cuzco)")
-
-        Curandero = self.societal = wx.checkbox(self, -1, "Curandero/Curandera")
-
-        HelperSacristan = self.societal = wx.checkbox(self, -1, "Helper/Sacristan (Ex; Yanapac)")
-
-        ChichaAsuacAccacMaker = self.societal = wx.checkbox(self, -1, "Chicha/Asuac/ Accac Maker")
-
-        ChacraLandGuardian = self.societal = wx.checkbox(self, -1, "Chacra/ Land Guardian (Ex: Ministros de los Parianas)")
-
-        BloodsuckersDeathDealersCaptains = self.societal = wx.checkbox(self, -1, "Bloodsuckers/ Death Dealers/ captains (Ex: Runapmícuc/ Cauchus)")
+        Consulter                        = self.societal = wx.CheckBox(self, -1, "Consulter")
+        Huaca                            = self.societal = wx.CheckBox(self, -1, "Huaca")
+        Malqui                           = self.societal = wx.CheckBox(self, -1, "Malqui/Munaos")
+        Lightning                        = self.societal = wx.CheckBox(self, -1, "Lightning")
+        Sun                              = self.Societal = wx.CheckBox(self, -1, "Sun")
+        Capycocha                        = self.Societal = wx.CheckBox(self, -1, "Capycocha")
+        OtherConsulter                   = self.Societal = wx.CheckBox(self, -1, "Other")
+        Diviners                         = self.societal = wx.CheckBox(self, -1, "Diviners")
+        Spiders                          = self.societal = wx.CheckBox(self, -1, "Spiders")
+        Molle                            = self.societal = wx.CheckBox(self, -1, "Molle")
+        Love                             = self.societal = wx.CheckBox(self, -1, "Love")
+        LostThings                       = self.Societal = wx.CheckBox(self, -1, "Lost things")
+        Mushrooms                        = self.Societal = wx.CheckBox(self, -1, "Mushrooms")
+        CuyExaminers                     = self.societal = wx.CheckBox(self, -1, "Cuy Examiners")
+        Purpose                          = self.societal = wx.CheckBox(self, -1, "Purpose")
+        Curer                            = self.societal = wx.CheckBox(self, -1, "Curer")
+        Confessor                        = self.societal = wx.CheckBox(self, -1, "Confessor")
+        Curandero                        = self.societal = wx.CheckBox(self, -1, "Curandero/\nCurandera")
+        HelperSacristan                  = self.societal = wx.CheckBox(self, -1, "Helper/\nSacristan")
+        ChichaAsuacAccacMaker            = self.societal = wx.CheckBox(self, -1, "Chicha/Asuac/\n Accac Maker")
+        ChacraLandGuardian               = self.societal = wx.CheckBox(self, -1, "Chacra/\n Land Guardian ")
+        BloodsuckersDeathDealersCaptains = self.societal = wx.CheckBox(self, -1, "Bloodsuckers/\n Death Dealers/\n captains")
 
 
 
@@ -50,15 +41,30 @@ class MatrixPanel(wx.Panel):
         space = 6
         infoSizer = wx.GridBagSizer(hgap=space, vgap=space)
         
-        infoSizer.Add(label1,   (1,0))        
-        infoSizer.Add(Consulter,  (1,1))
-        infoSizer.Add(Huaca, (2,1))
-        infoSizer.Add(Malqui, (3,1))
-        infoSizer.Add(Lightning, (1,2))
-        infoSizer.Add(Sun,  (2,2))
-        infoSizer.Add(Capycocha,     (3,2))
-        
-           
+        infoSizer.Add(label1                          ,(1,0))        
+        infoSizer.Add(Consulter                       ,(2,1))
+        infoSizer.Add(Huaca                           ,(3,2))
+        infoSizer.Add(Malqui                          ,(4,2))
+        infoSizer.Add(Lightning                       ,(5,2))
+        infoSizer.Add(Sun                             ,(6,2))
+        infoSizer.Add(Capycocha                       ,(7,2))
+        infoSizer.Add(OtherConsulter                  ,(8,2))
+        infoSizer.Add(Diviners                        ,(10,1))        
+        infoSizer.Add(Spiders                         ,(11,1))
+        infoSizer.Add(Molle                           ,(12,1))
+        infoSizer.Add(Love                            ,(13,1))
+        infoSizer.Add(LostThings                      ,(14,1))
+        infoSizer.Add(Mushrooms                       ,(15,1))
+        infoSizer.Add(CuyExaminers                    ,(17,1))        
+        infoSizer.Add(Purpose                         ,(18,2))        
+        infoSizer.Add(Curer                           ,(20,1))        
+        infoSizer.Add(Confessor                       ,(22,1))        
+        infoSizer.Add(Curandero                       ,(24,1))        
+        infoSizer.Add(HelperSacristan                 ,(26,1))        
+        infoSizer.Add(ChichaAsuacAccacMaker           ,(28,1))        
+        infoSizer.Add(ChacraLandGuardian              ,(30,1))        
+        infoSizer.Add(BloodsuckersDeathDealersCaptains,(32,1))        
+
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(infoSizer, 1, wx.EXPAND)
 
